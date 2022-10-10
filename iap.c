bce27b5da76c49bdd8c01417537ee12f427c32d7
2023181377@qq.com <joker>
2022-10-10 23:36:32 +0800

    [功能]AB分区升级

--- iap.c
+++ iap.c
@@ -2,52 +2,179 @@
 #include "iap_config.h"
 #include "gd32f30x_libopt.h"
 
-fmc_state_enum flash_write_word(uint32_t address, uint32_t data)
+uint32_t flash_read_word(uint32_t address)
 {
-	  fmc_state_enum state;
-		
-    fmc_unlock();
-		state = fmc_word_program(address, data);
-	
-    fmc_flag_clear(FMC_FLAG_BANK0_END);
-    fmc_flag_clear(FMC_FLAG_BANK0_WPERR);
-    fmc_flag_clear(FMC_FLAG_BANK0_PGERR);
-	
-	  fmc_lock();
-		
-		return state;
+	  return *((uint32_t*)address);
 }
 
-uint32_t APP_Jump_Address(void)
+uint16_t flash_read_dword(uint32_t address)
 {
-	 uint32_t jump_address = *((uint32_t*)APP_ADDR_ADDRESS);
+	  return *((uint16_t*)address);
+}
+
+uint8_t flash_read_byte(uint32_t address)
+{
+	  return *((uint8_t*)address);
+}
+
+void flash_read_buffer(uint32_t address, uint8_t *data,uint32_t data_len)
+{
+	  uint32_t* ptr = (uint32_t*)data;
+		for(int i = 0;i < data_len/4;i++){
+				*ptr++ = (*((volatile uint32_t*)(uint32_t)address));
+			  address += 4;
+		}
+		
+		uint8_t* byte_ptr = (uint8_t*)ptr;
+		
+		for(int j = 0;j < data_len%4;j++)
+		{
+			 *byte_ptr++ = (*((volatile uint8_t*)(uint32_t)address++));
+		}
+}
+
+uint32_t flash_write_buffer(uint32_t address, uint8_t *data,uint32_t data_len)
+{
+	  fmc_state_enum fmc_state;
+	  uint32_t ret = 0;
+	  uint8_t remainder = 0;
+	  uint32_t write_value = 0;
+	  uint16_t half_word = 0;
+	  
+	  fmc_unlock();
+
+		remainder = address % 2;
 	
-	 if((jump_address == APP_A_ADDRESS) || (jump_address == APP_B_ADDRESS))
-	 {
-		  return jump_address;
-	 }
-	 printf("APP_ADDR doesn't exist,the default address is APP_A_ADDRESS\r\n");
-	 flash_write_word(APP_ADDR_ADDRESS,APP_A_ADDRESS);		//用于第一次ota升级时，A分区程序判断当前程序所处地址
-	 
-	 return APP_A_ADDRESS;			//初次烧录，app address未写入，用于直接跳转至A分区
+		address -= remainder;
+	
+		for(int i = (address / FMC_PAGE_SIZE);i <= ((address + data_len) / FMC_PAGE_SIZE);i++)
+		{
+			 fmc_page_erase(i * FMC_PAGE_SIZE);
+		}
+		
+	  if(remainder){
+			  half_word = data[0] << 8;
+			  fmc_state = fmc_halfword_program(address, half_word);
+				
+				fmc_flag_clear(FMC_FLAG_BANK0_END);
+				fmc_flag_clear(FMC_FLAG_BANK0_WPERR);
+				fmc_flag_clear(FMC_FLAG_BANK0_PGERR);
+			
+				if(fmc_state != FMC_READY){
+				    ret = address;
+					goto exit;
+				}
+			
+			  address += 2;
+			  data_len -= remainder;
+			  data++;
+		}
+		
+		for(int j = 0;j < data_len/4;j++)
+		{
+			  write_value = (data[4*j + 0] << 0) | (data[4*j + 1] << 8) | (data[4*j + 2] << 16) | (data[4*j + 3] << 24);
+			  
+				fmc_state = fmc_word_program(address, write_value);
+			  address += 4;
+			
+				fmc_flag_clear(FMC_FLAG_BANK0_END);
+				fmc_flag_clear(FMC_FLAG_BANK0_WPERR);
+				fmc_flag_clear(FMC_FLAG_BANK0_PGERR);
+			
+				if(fmc_state != FMC_READY){
+				    ret = address;
+					goto exit;
+				}
+		}
+		
+		if(data_len%4 != 0)
+		{
+			 switch(data_len%4)
+			 {
+				 case 1:
+					 write_value = data[data_len-1];
+					 break;
+				 case 2:
+					 write_value = data[data_len-2] | (data[data_len-1] << 8);
+					 break;
+				 case 3:
+					 write_value = data[data_len-3] | (data[data_len-2] << 8) | (data[data_len-1] << 16);
+					 break;
+				 default:
+					 write_value = 0;
+					 break;
+			 }
+				fmc_state = fmc_word_program(address, write_value);
+			
+				fmc_flag_clear(FMC_FLAG_BANK0_END);
+				fmc_flag_clear(FMC_FLAG_BANK0_WPERR);
+				fmc_flag_clear(FMC_FLAG_BANK0_PGERR);
+
+				if(fmc_state != FMC_READY){
+				    ret = address;
+					goto exit;
+				}			 
+		}
+		
+exit:
+	  fmc_lock();
+		return ret;
+}
+
+void Write_APP_Size(uint32_t File_Bytes)
+{
+	 flash_write_buffer(APP_SIZE_ADDRESS, (uint8_t*)&File_Bytes,sizeof(uint32_t));
+}
+
+uint8_t Update_Init_Flag(void)
+{
+	uint32_t Valid_Flag = 0xAABBCCDD;
+	uint32_t Current_Flag = flash_read_word(INFO_ADDRESS);
+	if(Current_Flag != Valid_Flag)
+	{
+		 Write_APP_Size(0);
+		 flash_write_buffer(INFO_ADDRESS, (uint8_t*)&Valid_Flag,sizeof(uint32_t));
+		 return 0;		//first init
+	}
+	return 1;
 }
 
 int32_t Jump_to_APP(void)
 {
-		uint32_t jump_addr = APP_Jump_Address();
-		printf("jump_addr = 0x%x\r\n",jump_addr);
-	  
-		if (0x20000000 == ((*(volatile uint32_t*)jump_addr) & 0x2FFE0000))
+		if (0x20000000 == ((*(volatile uint32_t*)APP_ADDRESS) & 0x2FFE0000))
 		{
-			  printf("Now Jump To APP\r\n");
-				usart_disable(USART0);
+			  uint8_t succeed = 1;
+			  uint8_t init_flag = Update_Init_Flag();
 			  
-				Jump_To_ADDR_t Jump_To_Application = (Jump_To_ADDR_t)(*(volatile uint32_t*)(jump_addr + 4));
+			  uint32_t file_size = flash_read_word(APP_SIZE_ADDRESS);
 			
-				uint32_t offset = *((volatile uint32_t*)APP_ADDR_ADDRESS) - FLASH_BASE_ADDR;
+				while((init_flag != 0) && (file_size <= APP_FLASH_SIZE) && (file_size > 0))	//not first init
+				{
+					 flash_write_buffer(APP_ADDRESS, (uint8_t*)APP_BACKUP_ADDRESS,file_size);
+					
+					 for(int i = 0;i < file_size;i++)
+					 {
+							if(*((volatile uint8_t *)(APP_ADDRESS+i)) != *((volatile uint8_t *)(APP_BACKUP_ADDRESS+i)))
+							{
+								 succeed = 0;
+								 break;
+							}
+					 }
+					 
+					 if(succeed){
+							Write_APP_Size(0);
+					 }
+					 file_size = flash_read_word(APP_SIZE_ADDRESS);
+				}
+			  
+				usart_disable(USART0);
+				
+				Jump_To_ADDR_t Jump_To_Application = (Jump_To_ADDR_t)(*(volatile uint32_t*)(APP_ADDRESS + 4));
+			
+				uint32_t offset = APP_ADDRESS - FLASH_BASE_ADDR;
 				nvic_vector_table_set(NVIC_VECTTAB_FLASH, offset);
 			  
-				__set_MSP(*(volatile uint32_t*) jump_addr);
+				__set_MSP(*(volatile uint32_t*) APP_ADDRESS);
 				Jump_To_Application();
 		}
 		return 1;
--- iap_config.h
+++ iap_config.h
@@ -1,33 +1,24 @@
 #ifndef IAP_CONFIG_H_
 #define IAP_CONFIG_H_
 
-/* 
-	256KB FLASH
-	#define FMC_PAGE_NUM           	((uint16_t)0x80U)
-	#define FMC_PAGE_SIZE           ((uint16_t)0x800U)
-	#define FMC_WRITE_START_ADDR    ((uint32_t)0x08000000U)
-	#define FMC_WRITE_END_ADDR      ((uint32_t)0x0803FFFFU)
-*/
-
+#define FMC_PAGE_NUM					((uint16_t)0x80U)
+#define FMC_PAGE_SIZE					((uint32_t)0x00000800U)
 
 #define FLASH_TOTAL_SIZE 			((uint32_t)0x40000U)
-
-#define APP_FLASH_SIZE         APP_A_FLASH_SIZE
 
 #define FLASH_BASE_ADDR 			((uint32_t)0x08000000U)
 
 #define IAP_ADDRESS 					((uint32_t)0x08000000U)
-#define APP_A_ADDRESS 				((uint32_t)0x08004000U)
-#define APP_B_ADDRESS 				((uint32_t)0x08021000U)
-#define APP_ADDR_ADDRESS			((uint32_t)0x0803E000U)
+#define APP_ADDRESS 					((uint32_t)0x08004000U)
+#define APP_BACKUP_ADDRESS 		((uint32_t)0x08021000U)
+#define APP_SIZE_ADDRESS			((uint32_t)0x0803E000U)
 #define INFO_ADDRESS 					((uint32_t)0x0803E800U)
 #define FLASH_END_ADDR 				((uint32_t)0x0803FFFFU)
 
-#define IAP_FLASH_SIZE 				((uint32_t)(APP_A_ADDRESS - IAP_ADDRESS))
-#define APP_A_FLASH_SIZE 			((uint32_t)(APP_B_ADDRESS - APP_A_ADDRESS))
-#define APP_B_FLASH_SIZE 			((uint32_t)(APP_ADDR_ADDRESS - APP_B_ADDRESS))
-#define APP_ADDR_FLASH_SIZE 	((uint32_t)(INFO_ADDRESS - APP_ADDR_ADDRESS))
+#define IAP_FLASH_SIZE 				((uint32_t)(APP_ADDRESS - IAP_ADDRESS))
+#define APP_FLASH_SIZE 				((uint32_t)(APP_BACKUP_ADDRESS - APP_ADDRESS))
+#define APP_BACKUP_FLASH_SIZE	((uint32_t)(APP_SIZE_ADDRESS - APP_BACKUP_ADDRESS))
+#define APP_SIZE_FLASH_SIZE 	((uint32_t)(INFO_ADDRESS - APP_SIZE_ADDRESS))
 #define INFO_FLASH_SIZE 			((uint32_t)(FLASH_END_ADDR - INFO_ADDRESS + 1))
-
 
 #endif

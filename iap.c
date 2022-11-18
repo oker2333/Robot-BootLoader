8ff8ab7886e8f8184c0882d17e5f3feecf328704
2023181377@qq.com <joker>
2022-11-18 23:35:20 +0800

    [优化]删除INIT_FLAG标志

--- iap.c
+++ iap.c
@@ -123,20 +123,6 @@ exit:
 		return ret;
 }
 
-uint8_t Update_Init_Flag(void)
-{
-	uint32_t valid_flag = 0xAABBCCDD;
-	uint32_t current_flag = flash_read_word(INIT_FLAG_ADDRESS);
-	if(current_flag != valid_flag)
-	{
-		 uint32_t jump_addr = APP_ADDRESS_A;
-		 flash_write_buffer(JUMP_ADDR_ADDRESS, (uint8_t*)&jump_addr,sizeof(uint32_t));
-		 flash_write_buffer(INIT_FLAG_ADDRESS, (uint8_t*)&valid_flag,sizeof(uint32_t));
-		 return 0;		//first init
-	}
-	return 1;
-}
-
 void Update_Boot_Ver(void)
 {
 	  uint16_t Boot_Version = ((MAJOR_VERSION << 8) | MINOR_VERSION);
@@ -150,8 +136,6 @@ void Update_Boot_Ver(void)
 void Jump_to_APP(void)
 {
 		Update_Boot_Ver();
-
-	  Update_Init_Flag();
 	
 	  uint32_t jump_address;
 		flash_read_buffer(JUMP_ADDR_ADDRESS,(uint8_t*)&jump_address,sizeof(uint32_t));
